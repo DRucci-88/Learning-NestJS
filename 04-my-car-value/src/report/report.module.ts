87113491f1c659075eead40eb2eb22b20aa292de
DRucci-88 <d.rucci.2001@gmail.com>
2024-08-24 12:00:10 +0700

    04 Creating and Repository

--- 04-my-car-value/src/report/report.module.ts
+++ 04-my-car-value/src/report/report.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 @Module({
   controllers: [ReportController],
-  providers: [ReportService]
+  providers: [ReportService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Report
+    ]),
+  ]
 })
-export class ReportModule {}
+export class ReportModule { }
