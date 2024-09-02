b7669089c27c77f0d1d8d50ce4187d6fe36fac9c
DRucci-88 <d.rucci.2001@gmail.com>
2024-09-03 00:25:16 +0700

    06 Typescript Help with Mocks

--- 06-my-car-value-testing/src/user/auth.service.spec.ts
+++ 06-my-car-value-testing/src/user/auth.service.spec.ts
@@ -1,16 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
     // Create testing DI Container
     // and some fake implementation of some other classes
 
     // Create a fake copy of the user service
-    const fakeUserService = {
+    const fakeUserService: Partial<UserService> = {
         find: () => Promise.resolve([]),
         create: (hobby: string, email: string, password: string) =>
-            Promise.resolve({ id: 1, hobby, email, password }),
+            Promise.resolve({ id: 1, hobby, email, password } as User),
     };
 
     // Create the module
