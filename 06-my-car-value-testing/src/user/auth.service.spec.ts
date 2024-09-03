9eb7836cdecfc6ecc3e95a509936674bc0f60df6
DRucci-88 <d.rucci.2001@gmail.com>
2024-09-03 16:08:45 +0700

    06 Ensuring Password Gets Hashed

--- 06-my-car-value-testing/src/user/auth.service.spec.ts
+++ 06-my-car-value-testing/src/user/auth.service.spec.ts
@@ -3,30 +3,50 @@ import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-    // Create testing DI Container
-    // and some fake implementation of some other classes
+let authService: AuthService;
 
-    // Create a fake copy of the user service
-    const fakeUserService: Partial<UserService> = {
-        find: () => Promise.resolve([]),
-        create: (hobby: string, email: string, password: string) =>
-            Promise.resolve({ id: 1, hobby, email, password } as User),
-    };
+describe('AuthService', () => {
+    beforeEach(async () => {
+        // Create testing DI Container
+        // and some fake implementation of some other classes
 
-    // Create the module
-    const module: TestingModule = await Test.createTestingModule({
-        providers: [
-            AuthService,
-            {
-                provide: UserService,
-                useValue: fakeUserService,
-            },
-        ],
-    }).compile();
+        // Create a fake copy of the user service
+        const fakeUserService: Partial<UserService> = {
+            find: () => Promise.resolve([]),
+            create: (hobby: string, email: string, password: string) =>
+                Promise.resolve({ id: 1, hobby, email, password } as User),
+        };
 
-    // DI Container will create a new instance with all of its different dependencies already initialized
-    const authServive = module.get(AuthService);
+        // Create the module
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UserService,
+                    useValue: fakeUserService,
+                },
+            ],
+        }).compile();
 
-    expect(authServive).toBeDefined();
+        // DI Container will create a new instance with all of its different dependencies already initialized
+        authService = module.get(AuthService);
+    });
+
+    it('can create an instance of auth service', async () => {
+        expect(authService).toBeDefined();
+    });
+
+    it('creates a new user with salted and hashed password', async () => {
+        const password = 'lepassword';
+        const user = await authService.signup(
+            'ternak lele',
+            'le@gmail.com',
+            password,
+        );
+
+        expect(user.password).not.toEqual(password);
+        const [salt, hash] = user.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+    });
 });
