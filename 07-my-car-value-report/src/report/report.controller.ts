5610e780bdcb4bee272df12c322bd90e466c037e
DRucci-88 <d.rucci.2001@gmail.com>
2024-09-09 13:40:30 +0700

    07 Report Create Dto Controller Service

--- 07-my-car-value-report/src/report/report.controller.ts
+++ 07-my-car-value-report/src/report/report.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { ReportService } from './report.service';
+import { AuthGuard } from 'src/guard/auth.guard';
 
 @Controller('report')
-export class ReportController {}
+export class ReportController {
+    constructor(private readonly reportService: ReportService) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() body: CreateReportDto) {
+        return this.reportService.create(body);
+    }
+}
--- 07-my-car-value-report/src/report/report.service.ts
+++ 07-my-car-value-report/src/report/report.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { plainToInstance } from 'class-transformer';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+    constructor(
+        @InjectRepository(Report)
+        private readonly reportRepo: Repository<Report>,
+    ) {}
+
+    create = (dto: CreateReportDto) => {
+        // const report: DeepPartial<Report> = this.reportRepo.create(dto);
+
+        const report: DeepPartial<Report> = plainToInstance(Report, dto);
+
+        return this.reportRepo.save(report);
+    };
+}
